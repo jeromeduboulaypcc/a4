2dc3673cd63c40def3b009e1f03178ce1cb60c2e
jerome.duboulay <jerome.duboulay@pcc.edu>
2017-10-24 22:54:32 -0700

    added main method that prints Hello World

--- src/edu/pcc/cis/a4/a4.java
+++ src/edu/pcc/cis/a4/a4.java
@@ -1,4 +1,18 @@
 package edu.pcc.cis.a4;
+/**
+ * Developer Tools Assignment
+ * by Jerome DuBoulay
+ * Version 10-24-2017
+*/
 
 public class a4 {
+    /**
+     * Print statements
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("This is project a4 for CIS 233J");
+        System.out.println("by Jerome Du Boulay")
+    }
 }
